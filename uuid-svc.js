8bf1e86a8d9a8d54f7d9314395ec737d7f3f81b2
Daniel Lamb <daniellmb@hotmail.com>
2014-03-13 08:02:53 -0700

    Update uuid-svc.js

--- uuid-svc.js
+++ uuid-svc.js
@@ -8,14 +8,14 @@ angular.module("uuid", []).factory("rfc4122", function () {
     return {
         v4: function () {
             // get currrent time
-            var time = new Date().getTime();
+            var time = new Date().getTime(), sixteen = 16;
             // create UUID by replacing the template string
             return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (match) {
-                var remainder = (time + 16 * Math.random()) % 16 | 0;
+                var remainder = (time + sixteen * Math.random()) % sixteen | 0;
                 // update for next match
-                time = Math.floor(time / 16);
+                time = Math.floor(time / sixteen);
                 // check matched character
-                return (match == "x" ? remainder : remainder & 7 | 8).toString(16);
+                return (match == "x" ? remainder : remainder & 7 | 8).toString(sixteen);
             });
         }
     };
